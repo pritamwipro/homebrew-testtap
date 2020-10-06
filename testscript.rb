35218ae427d13eb297419904bf8caf02623edf02
Pritam Ghosh <pritam.ghosh@bt.com>
2020-10-06 13:51:10 +0100

    add formula

---
+++ testscript.rb
@@ -0,0 +1,15 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Testscript < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/pritamwipro/testscript/archive/v1.0.0.tar.gz"
+  sha256 "5066f192aa3915123875019b68cdcd407c9f4e09d5600c0cfa34a5cc7a7364b2"
+  license ""
+
+  def install
+	bin.install "testscript"
+  end
+
+end
